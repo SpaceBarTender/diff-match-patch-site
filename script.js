3bf8f2ab71d5ce1db786fe87ee043e37a927ba9b
SpaceBarTender <adumas97@gmail.com>
2024-01-20 04:47:41 -0600

    Insert comments to script.js to speficy major sections

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+
+//Initialize const for txt inputs
+
 const txt1 = document.getElementById("text1");
 
 const txt2 = document.getElementById("text2");
 
 const btn1 = document.getElementById("btn1");
+
+//Initialize const for outputs TODO change output1 to diff_match
 
 const out1 = document.getElementById("output1");
 
@@ -10,12 +15,16 @@ const txtout1 = document.getElementById("txtout1");
 
 const txtout2 = document.getElementById("txtout2");
 
+//Function to return values of inputs and diff-match
+
 function getDiffAndDisplay() {
     const diff2 = diff_match_patch.prototype.diff_prettyHtml(diff_match_patch.prototype.diff_main(txt1.value, txt2.value));
   
     txtout1.innerText = "First block: " + txt1.value;
     txtout2.innerText = "Second block: " + txt2.value;
     out1.innerHTML = "Diff-match: " + diff2;
+
+
   }
     
   
