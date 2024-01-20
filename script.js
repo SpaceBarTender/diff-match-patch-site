36094c7b90785735a62911f4a365107395f33d6c
SpaceBarTender <adumas97@gmail.com>
2024-01-20 04:22:53 -0600

    Declare and set const values for txt1 and 2, button, output, and output verson of txt2 and 2

---
+++ script.js
@@ -0,0 +1,11 @@
+const txt1 = document.getElementById("text1");
+
+const txt2 = document.getElementById("text2");
+
+const btn1 = document.getElementById("btn1");
+
+const out1 = document.getElementById("output1");
+
+const txtout1 = document.getElementById("txtout1");
+
+const txtout2 = document.getElementById("txtout2");
