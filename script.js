c55bb53db3494277db5652a25539036564ed9edc
SpaceBarTender <adumas97@gmail.com>
2024-01-20 04:24:20 -0600

    Create funciton getDiffAndDisplay() and add event listener for button for that function

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ const out1 = document.getElementById("output1");
 const txtout1 = document.getElementById("txtout1");
 
 const txtout2 = document.getElementById("txtout2");
+
+function getDiffAndDisplay() {
+    const diff2 = diff_match_patch.prototype.diff_prettyHtml(diff_match_patch.prototype.diff_main(txt1.value, txt2.value));
+  
+    txtout1.innerText = "First block: " + txt1.value;
+    txtout2.innerText = "Second block: " + txt2.value;
+    out1.innerHTML = diff2;
+  }
+    
+  
+btn1.addEventListener('click', getDiffAndDisplay);
